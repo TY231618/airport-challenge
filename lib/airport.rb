cd9a05114e97fcf116db5edf44dadaa75bc9f955
Tony Young <ty_training@hotmail.co.uk>
2015-12-03 20:47:45 +0000

    User story 3: cannot land plane when at capacity

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,6 +1,13 @@
 class Airport
 
+  def initialize(capacity)
+    @capacity = capacity
+    @grounded_planes = []
+  end
+
   def land(plane)
+    raise 'Cannot land plane: airport is full' if @grounded_planes.length >= @capacity
+    @grounded_planes << plane
   end
 
   def take_off(plane)
--- spec/features/user_story_spec.rb
+++ spec/features/user_story_spec.rb
@@ -3,7 +3,7 @@ describe 'user stories' do
   # So planes can land safely at my airport
   # I would like to instruct a plane to land
   it 'instructs a plane to land at an airport'do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.land(plane) }.not_to raise_error
   end
@@ -12,8 +12,25 @@ describe 'user stories' do
   # So planes can take off safely from my airport
   # I would like to instruct a plane to take off
   it 'instructs a plane to take off from an airport' do
-    airport = Airport.new
+    airport = Airport.new(20)
     plane = Plane.new
     expect { airport.take_off(plane) }.not_to raise_error
   end
+
+  # As an airtraffic controller
+  # So that I can avoid collisions
+  # I want to prevent airplanes landing when my airport is full
+
+  it 'prevents planes landing when airport is full' do
+    airport = Airport.new(20)
+    plane = Plane.new
+    20.times do
+      airport.land(plane)
+    end
+    expect { airport.land(plane)}.to raise_error 'Cannot land plane: airport is full'
+  end
+
+
+
+
 end
